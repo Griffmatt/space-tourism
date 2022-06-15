d39200b96a850e1f210f34f5320c701a077351aa
Griffin <griffinmmatthews@gmail.com>
2022-06-14 20:00:54 -0400

    finished desktop home page

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -3,6 +3,19 @@ import React from 'react'
 function HomePage() {
   return (
     <div className="homePage">
+      <div className="homePageInfoWrapper">
+        <div className="homePageInfo">
+          <h5>SO, YOU WANT TO TRAVEL TO</h5>
+          <h1>SPACE</h1>
+          <p>Let’s face it; if you want to go to space, you might as well genuinely go to 
+            outer space and not hover kind of on the edge of it. Well sit back, and relax 
+            because we’ll give you a truly out of this world experience!
+          </p>
+        </div>
+        <div className="homePageButton">
+          <button><h4>EXPLORE</h4></button>
+        </div>
+      </div>
     </div>
   )
 }
