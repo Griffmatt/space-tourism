7e6d04a72c5f9f97a455afbd3c1738365f2bc6d4
Griffin <griffinmmatthews@gmail.com>
2022-06-20 17:48:36 -0400

    tech page adaptive

--- src/components/pages/TechnologyPage.js
+++ src/components/pages/TechnologyPage.js
@@ -1,19 +1,29 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Data from '../../data.json'
 
 function TechnologyPage() {
   let tech = Data.technology
   const [active, setActive] = useState(0)
+  const [image, setImage] = useState()
   const navDots = [1, 2, 3]
 
-  console.log(tech)
+  useEffect(()=>{
+    if(window.innerWidth < 1023){
+      setImage(tech[active].images.landscape)
+      return
+    }
+    setImage(tech[active].images.portrait)
+
+  }, [active, tech])
+
+
   return (
     <div className="technologyPage">
       <h5 className="pageTitle"><span>03</span>SPACE LAUNCH 101</h5>
       <div className="techNav">
         {navDots.map(dot=>{
           return(
-            <h4 className={`techDot ${active===dot-1?"active":""}`} onClick={()=>setActive(dot-1)}>{dot}</h4>
+            <h4 key={dot} className={`techDot ${active===dot-1?"active":""}`} onClick={()=>setActive(dot-1)}>{dot}</h4>
           )
         })}
       </div>
@@ -22,7 +32,7 @@ function TechnologyPage() {
         <h3>{tech[active].name}</h3>
         <p>{tech[active].description}</p>
       </div>
-      <img src={tech[active].images.portrait} alt={tech[active].name}/>
+      <img src={image} alt={tech[active].name}/>
     </div>
   )
 }
