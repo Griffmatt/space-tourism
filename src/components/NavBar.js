762bd44e51506bba420f1b72f2370d68676a1b1e
Griffin <griffinmmatthews@gmail.com>
2022-06-21 19:01:49 -0400

    fully adaptive still need to make fully responsive

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {NavLink, Link} from 'react-router-dom'
 
 function NavBar() {
+
+  const [navHidden, setNavHidden] = useState({})
+
+  const handleNavOpen = ()=>{
+    setNavHidden({right:0})
+  }
+
+  const handleNavClose = ()=>{
+    setNavHidden({})
+  }
+
   return (
     <div className="navBar">
-        <Link to="/"><img src="/images/shared/logo.svg"/></Link>
+        <Link to="/"><img src="/images/shared/logo.svg" alt="Logo" className="navBarLogo"/></Link>
+        <img src="/images/shared/icon-hamburger.svg" className="navBarHamburger" alt="Hamburger Icon" onClick={handleNavOpen}/>
         <div className="navBarLine"/>
-        <div className="navBarListWrapper">
+        <div className="navBarListWrapper" style={navHidden}>
+          <img src="/images/shared/icon-close.svg" className="navBarClose" alt="Hamburger Icon" onClick={handleNavClose}/>
             <div className="navBarList">
                 <ul>
                     <li className="navText"><NavLink to="/"><span>00</span>  HOME</NavLink></li>
